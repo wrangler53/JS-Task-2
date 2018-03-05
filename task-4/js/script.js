0d274c66d0625d98a62b8065f1499b4813edb64c
Yaroslav Melnychuk <melnichukjarik@gmail.com>
2018-03-05 18:49:11 +0200

    Added Go to top functionality

---
+++ task-4/js/script.js
@@ -0,0 +1,13 @@
+$(window).scroll(() => {
+    // If page is scrolled more than 50px
+    if ($(this).scrollTop() >= 50) {        
+        $('#go-to-top-btn').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#go-to-top-btn').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#go-to-top-btn').click(() => {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 600);
+});
