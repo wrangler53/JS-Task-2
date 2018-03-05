0d27e50656297ffe5fdd541876ee3327b40fb171
Yaroslav Melnychuk <melnichukjarik@gmail.com>
2018-03-05 14:36:08 +0200

    Added isotope filtering logic

--- task-1/js/script.js
+++ task-1/js/script.js
@@ -2,3 +2,15 @@ $('.isotope-container').isotope({
     itemSelector: '.isotope-item',
     layoutMode: 'fitRows'
 });
+
+$('.menu-container ul li').click(function() {
+    $('.menu-container ul li').removeClass('active');
+    $(this).addClass('active');
+
+    var selector = $(this).attr('data-filter');
+
+    $('.isotope-container').isotope({
+        filter: selector
+    });
+    return false;
+})
